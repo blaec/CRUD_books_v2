96cdc2ca17928e6d7d9003638463a89c9d2ee7cb
blaec <odeskonst@yandex.ru>
2018-04-30 22:36:01 +0300

    added service, Spring new dependencies

---
+++ src/main/java/com/github/books/service/BookService.java
@@ -0,0 +1,16 @@
+package com.github.books.service;
+
+
+import com.github.books.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+    void addBook(Book book);
+    void updateBook(Book book);
+    void removeBook(int id);
+    void makeRead(boolean isUpdate, Book book);
+    Book getBookById(int id);
+    List<Book> listBooks();
+    Book getBookByName(String searchName);
+}
